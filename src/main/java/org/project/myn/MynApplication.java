d59d2f164253813e25d97270d3cdb5591a960a1d
yeseong31 <yeseong31@naver.com>
2022-05-04 22:31:22 +0900

    0504 Start MYN Project

---
+++ src/main/java/org/project/myn/MynApplication.java
@@ -0,0 +1,13 @@
+package org.project.myn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/myn/MynApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.myn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
