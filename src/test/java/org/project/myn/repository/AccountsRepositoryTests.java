9991fa702c70f0c2f4f8958b157fe8535651687f
yeseong31 <yeseong31@naver.com>
2022-05-11 17:55:24 +0900

    0511 Querydsl test

--- src/test/java/org/project/myn/repository/AccountsRepositoryTests.java
+++ src/test/java/org/project/myn/repository/AccountsRepositoryTests.java
@@ -1,15 +1,20 @@
 package org.project.myn.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.project.myn.entity.Accounts;
 import org.project.myn.entity.AccountsRole;
+import org.project.myn.entity.QAccounts;
 import org.project.myn.security.SHA256;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.stream.IntStream;
@@ -53,10 +58,29 @@ public class AccountsRepositoryTests {
     }
 
     // Querydsl - 단일 항목 검색 테스트
+    @Transactional
     @Test
     public void testQuery1() {
         Pageable pageable = PageRequest.of(0, 10, Sort.by("id").descending());
 
+        // 1) 동적 처리를 위해 Q도메인 클래스 활용
+        QAccounts qAccounts = QAccounts.accounts;
+
+        String keyword = "1";
+
+        // 2) BooleanBuilder는 where 문에 들어가는 조건들을 넣어주는 컨테이너
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 3) 원하는 조건은 필드 값과 같이 결합하여 생성 (com.querydsl.core.types.Predicate 타입)
+        BooleanExpression expression = qAccounts.username.contains(keyword);
+
+        // 4) 조건을 키워드와 결합
+        builder.and(expression);
+
+        // 5) QuerydslPredicateExcutor 인터페이스의 findAll() 등의 메서드를 사용할 수 있음
+        Page<Accounts> result = accountsRepository.findAll(builder, pageable);
+
+        result.stream().forEach(System.out::println);
     }
 
 }
