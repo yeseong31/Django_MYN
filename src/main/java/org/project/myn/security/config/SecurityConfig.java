4f022ec5c90dbf396158856d302fd930828af582
yeseong31 <yeseong31@naver.com>
2022-05-19 22:28:06 +0900

    0519 서버 접근 URL 변경

--- src/main/java/org/project/myn/security/config/SecurityConfig.java
+++ src/main/java/org/project/myn/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,10 +16,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // 사용자 계정: user1, 1111(인코딩 결과)
-        auth.inMemoryAuthentication().withUser("user1")
-                .password("$2a$10$q0PodL0NQv4djdq3PuWLc.tSihSgLQSwRQ1tx5IMSuaKjd1YM4mvy")
-                .roles("USER");
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/account/list").permitAll();
+        http.formLogin();
+        http.csrf().disable();
+
     }
 }
