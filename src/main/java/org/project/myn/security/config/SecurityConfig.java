55359158322aac1b92f48191487e9bfc4ff60aad
yeseong31 <yeseong31@naver.com>
2022-05-25 17:35:22 +0900

    0525 Remember Me 설정(일반 로그인)

--- src/main/java/org/project/myn/security/config/SecurityConfig.java
+++ src/main/java/org/project/myn/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.project.myn.security.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.project.myn.security.handler.ClubLoginSuccessHandler;
+import org.project.myn.security.service.ClubUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
@@ -43,7 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         // 소셜 로그인
         http.oauth2Login().successHandler(successHandler());
-
+        // 자동 로그인
+        http.rememberMe().tokenValiditySeconds(60 * 60 * 24 * 7).userDetailsService(userDetailsService);  // 7days
     }
 
     @Bean
