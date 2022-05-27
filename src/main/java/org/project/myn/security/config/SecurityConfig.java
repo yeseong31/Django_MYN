b09e81f9e9fd9fdb0a51a342d98bf5e7527ab510
yeseong31 <yeseong31@naver.com>
2022-05-27 13:19:28 +0900

    0527 OncePerRequestFilter 적용

--- src/main/java/org/project/myn/security/config/SecurityConfig.java
+++ src/main/java/org/project/myn/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.project.myn.security.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.project.myn.security.filter.ApiCheckFilter;
 import org.project.myn.security.handler.ClubLoginSuccessHandler;
 import org.project.myn.security.service.ClubUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,23 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .csrf().disable()
-
-                // Spring Security HSTS
-                .headers()
-                    .httpStrictTransportSecurity()
-                        .maxAgeInSeconds(31536000)    // 브라우저가 HSTS 정책을 적용할 시간 설정
-                        .includeSubDomains(true)      // 도메인의 서브 도메인에도 HSTS 설정을 적용
-                        .preload(true);               // HSTS 헤더가 없더라도 HTTP 요청을 HTTPS로 강제 변환하여 전송함
-
-//                .and()
-//                    .authorizeRequests()
-//                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-//                    .antMatchers("/sample/all").permitAll()
-//                    .antMatchers("/sample/member").hasRole("USER")
-//                .and()
-//                    .logout()
-//                        .logoutSuccessUrl("/");
+                .csrf().disable();
 
         // 인가 및 인증에 문제가 있을 때 로그인 화면 출력
         http.formLogin();
@@ -57,5 +42,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public ClubLoginSuccessHandler successHandler() {
         return new ClubLoginSuccessHandler(passwordEncoder());
+    }
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter() {
+        return new ApiCheckFilter();
     }
 }
