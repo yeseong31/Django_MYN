a280156de5a3d708d990fcacd8f74326722782dc
yeseong31 <yeseong31@naver.com>
2022-05-27 11:00:01 +0900

    0527 사용자 등록 로직 추가

--- src/main/java/org/project/myn/service/AccountServiceImpl.java
+++ src/main/java/org/project/myn/service/AccountServiceImpl.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.project.myn.dto.AccountDTO;
 import org.project.myn.entity.Account;
+import org.project.myn.entity.ClubMember;
+import org.project.myn.entity.ClubMemberRole;
 import org.project.myn.repository.AccountRepository;
+import org.project.myn.repository.ClubMemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +20,25 @@ import java.util.stream.Collectors;
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
+    private final ClubMemberRepository clubMemberRepository;
 
     @Override
-    public String register(AccountDTO dto) {
-        Account account = dtoToEntity(dto);
-        accountRepository.save(account);
-        return account.getClubMember().getEmail();
+    public Long register(AccountDTO accountDTO) {
+        // Account 개설 전 사용자 회원가입이 완료된 회원인지 판단해야 함
+        Optional<ClubMember> result = clubMemberRepository.findByEmail(accountDTO.getEmail());
+
+        // 해당 계정이 존재한다면 권한 추가 후 Account 개설
+        if (result.isPresent()) {
+            ClubMember clubMember = result.get();
+            clubMember.addMemberRole(ClubMemberRole.MEMBER);
+            clubMemberRepository.save(clubMember);
+
+            Account account = dtoToEntity(accountDTO);
+            accountRepository.save(account);
+            return account.getId();
+        }
+        // 그렇지 않으면 Account를 개설하지 않음
+        return null;
     }
 
     @Override
@@ -32,12 +48,12 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public void modify(AccountDTO dto) {
-        Long id = dto.getId();
+    public void modify(AccountDTO accountDTO) {
+        Long id = accountDTO.getId();
         Optional<Account> result = accountRepository.findById(id);
         if (result.isPresent()) {
             Account account = result.get();
-            account.changeUsername(dto.getUsername());
+            account.changeUsername(accountDTO.getUsername());
             accountRepository.save(account);
         }
     }
