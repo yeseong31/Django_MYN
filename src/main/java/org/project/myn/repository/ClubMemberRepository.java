a2e16aed1b595eaac74f16b95a1f43bf9a8d95f7
yeseong31 <yeseong31@naver.com>
2022-05-27 11:00:53 +0900

    0527 사용자 조회 기능 변경

--- src/main/java/org/project/myn/repository/ClubMemberRepository.java
+++ src/main/java/org/project/myn/repository/ClubMemberRepository.java
@@ -10,9 +10,12 @@ import java.util.Optional;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, String> {
 
-    // 사용자 이메일(아이디) 조회
+    // 사용자 이메일(아이디)과 소셜가입여부를 확인하여 정보 조회
     @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("select m from ClubMember m where m.fromSocial = :social and m.email = :email")
-    Optional<ClubMember> findByEmail(@Param("email") String email, @Param("social") boolean social);
+    Optional<ClubMember> findByEmailWithSocial(@Param("email") String email, @Param("social") boolean social);
 
+    @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select m from ClubMember m where m.email = :email")
+    Optional<ClubMember> findByEmail(@Param("email") String email);
 }
