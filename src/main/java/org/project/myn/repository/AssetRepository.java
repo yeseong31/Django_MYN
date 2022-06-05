7edd3e0b0c20967b1b271393454a59f99a324c5d
yeseong31 <yeseong31@naver.com>
2022-06-05 17:34:21 +0900

    0605 Asset Query문 수정

--- src/main/java/org/project/myn/repository/AssetRepository.java
+++ src/main/java/org/project/myn/repository/AssetRepository.java
@@ -16,7 +16,8 @@ public interface AssetRepository extends JpaRepository<Asset, Long> {
     @Query("select a, ac from Asset a, Account ac where a.id = :id and a.account.id = ac.id")
     List<Object[]> getAssetWithAccount(@Param("id") Long id);
 
-    @Query("select a, ac from Asset a left outer join Account ac on ac.id = a.account.id")
+    // '/explore'의 'New Assets'에 해당하는 정보: 가장 최근에 등록된 Asset 정보와 username 정보 조회
+    @Query("select a, ac.username from Asset a left outer join Account ac on ac.id = a.account.id")
     Page<Object[]> getNewAssets(Pageable pageable);
 
 }
