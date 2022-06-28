89e113478dc7a045e2d185c2d42f562339cf3747
예성 <yeseong31@naver.com>
2022-06-28 21:47:31 +0900

    0628 staticfiles 설정

--- config/settings.py
+++ config/settings.py
@@ -156,9 +156,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = ()
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
