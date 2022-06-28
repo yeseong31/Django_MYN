21e539a4ee27548f80a11e04bb46ecd5fcd925bd
예성 <yeseong31@naver.com>
2022-06-28 21:18:07 +0900

    0628 update settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,12 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from pathlib import Path
 
 import dj_database_url
 import environ
-
-from config import my_settings
 
 env = environ.Env()
 environ.Env.read_env()
