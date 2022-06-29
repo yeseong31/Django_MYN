44417b737c047d711cba019f061d973540026621
yeseong31 <yeseong31@naver.com>
2022-06-29 10:34:48 +0900

    0629 update settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,6 @@ import os
 import dj_database_url
 import environ
 
-from config import my_settings
-
 env = environ.Env()
 environ.Env.read_env()
 
@@ -27,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', my_settings.SECRET_KEY)
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,10 +99,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', my_settings.DB_NAME),
-        'USER': os.getenv('DB_USER', my_settings.DB_USER),
-        'PASSWORD': os.getenv('DB_PASSWORD', my_settings.DB_PASSWORD),
-        'HOST': os.getenv('DB_HOST', my_settings.DB_HOST),
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
