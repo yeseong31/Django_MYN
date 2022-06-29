d296302fa2d03962e2d4a433a51453d1d4755cad
yeseong31 <yeseong31@naver.com>
2022-06-29 12:33:48 +0900

    0629 staticfiles test(debug)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
