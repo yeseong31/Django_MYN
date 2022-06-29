39bb79cdcc3bb054f648be7299884b7aa0fb0211
yeseong31 <yeseong31@naver.com>
2022-06-29 12:44:28 +0900

    0629 heroku staticfiles test

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))  # False로 변경 시 staticfiles를 찾지 못 하는 문제가 있음
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
